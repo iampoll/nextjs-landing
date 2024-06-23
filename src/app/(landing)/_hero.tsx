3e376b630adcdb6ed7f3a995d249df37809676ac
n0wickk <memetitv01@gmail.com>
2024-06-23 11:06:49 +0800

    feat: :sparkles: add hero

---
+++ src/app/(landing)/_hero.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React from "react";
+
+import AnimatedGradientText from "@/components/magicui/animated-gradient-text";
+import { ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/magicui/hero-highlight";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+function Hero() {
+  return (
+    <div className="relative h-[2000px] overflow-hidden py-14 text-center">
+      <AnimatedGradientText>
+        🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-300" />{" "}
+        <span
+          className={cn(
+            `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
+          )}
+        >
+          Explore Our Demo Now
+        </span>
+        <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+      </AnimatedGradientText>
+
+      <h1 className="relative max-w-[46.5rem] text-balance bg-gradient-to-br from-black from-50% to-neutral-200/60 bg-clip-text pt-5 text-4xl font-semibold tracking-tighter text-transparent dark:text-white sm:text-7xl mx-auto md:px-4 md:py-2 md:text-center md:text-7xl lg:text-7xl my-4">
+        Student Information Management System
+      </h1>
+
+      <HeroHighlight>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+        >
+          <p className="text-balance max-w-[80ch] mx-auto tracking-tight inline">
+            Leverage the power of {""}
+          </p>
+          <Highlight className="text-balance max-w-[80ch] mx-auto tracking-tight inline font-medium">
+            modern technology
+          </Highlight>
+          <p className="text-balance max-w-[80ch] mx-auto tracking-tight inline">
+            {""} with our advanced SIMS, <br className="hidden md:block" />{" "}
+            providing intuitive tools for educators and seamless experiences for
+            students.
+          </p>
+        </motion.h1>
+      </HeroHighlight>
+
+      <section className="flex px-4 max-w-[500px] mx-auto mt-8">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({
+              size: "lg",
+              variant: "outline",
+            }),
+            "gap-2 whitespace-pre md:flex",
+            "group relative w-full gap-1 overflow-hidden  text-sm font-semibold tracking-tighter transition-all duration-150 ease-in-out hover:ring-2 hover:ring-neutral-300 hover:ring-offset-2 hover:ring-offset-inherit dark:hover:ring-primary dark:hover:ring-offset-primary mr-4"
+          )}
+        >
+          Learn More
+          <ChevronRight className="ml-1 size-4 flex-shrink-0 transition-all duration-300 ease-out group-hover:translate-x-1" />
+        </Link>
+
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({
+              variant: "default",
+              size: "lg",
+            }),
+            "gap-2 whitespace-pre md:flex",
+            "group relative w-full gap-1  text-sm font-semibold tracking-tighter ring-offset-inherit transition-all duration-150 ease-in-out hover:ring-2 hover:ring-primary hover:ring-offset-2 hover:ring-offset-current dark:hover:ring-neutral-50 ml-4"
+          )}
+        >
+          Get Started
+          <ChevronRight className="ml-1  size-4 flex-shrink-0 transition-all duration-300 ease-out group-hover:translate-x-1" />
+        </Link>
+      </section>
+    </div>
+  );
+}
+
+export default Hero;
--- src/config/site.ts
+++ src/config/site.ts
@@ -3,10 +3,10 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "SIMS",
   description: "Student Information Management System",
-  url: "https://sims-landing.vercel.app",
-  icon: "https://sims-landing.vercel.app/icon.svg",
-  logo: "https://sims-landing.vercel.app/logo.svg",
-  ogImage: "https://sims-landing.vercel.app/og.jpg",
+  url: "https://sims-landing-demo.vercel.app",
+  icon: "https://sims-landing-demo.vercel.app/icon.svg",
+  logo: "https://sims-landing-demo.vercel.app/logo.svg",
+  ogImage: "https://sims-landing-demo.vercel.app/og.jpg",
   links: {
     github: "https://github.com/n0Wickk/nextjs-landing",
   },

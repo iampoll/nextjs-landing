de6f3347d6e833bdd30bf5e5e6f8bba36a348e59
n0wickk <memetitv01@gmail.com>
2024-06-23 09:16:22 +0800

    chore: :bento: add icon and logo

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,6 +4,8 @@ export const siteConfig: SiteConfig = {
   name: "SIMS",
   description: "Student Information Management System",
   url: "https://sims-landing.vercel.app",
+  icon: "https://sims-landing.vercel.app/icon.svg",
+  logo: "https://sims-landing.vercel.app/logo.svg",
   ogImage: "https://sims-landing.vercel.app/og.jpg",
   links: {
     github: "https://github.com/n0Wickk/nextjs-landing",
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,8 @@ export type SiteConfig = {
   name: string;
   description: string;
   url: string;
+  icon: string;
+  logo: string;
   ogImage: string;
   links: {
     github: string;

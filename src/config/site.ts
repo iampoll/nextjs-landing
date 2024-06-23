b12e7b754bbf3dd0f30359e0f62894a94b35226e
n0wickk <memetitv01@gmail.com>
2024-06-23 09:07:07 +0800

    feat: :wrench: add site config

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "SIMS",
+  description: "Student Information Management System",
+  url: "https://sims-landing.vercel.app",
+  ogImage: "https://sims-landing.vercel.app/og.jpg",
+  links: {
+    github: "https://github.com/n0Wickk/nextjs-landing",
+  },
+};
